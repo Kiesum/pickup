1412b83fc44eccc07fb85cfd6b51e300682129c4
Kieran <kieran.summerfeldt@gmail.com>
2016-07-04 15:51:09 -0700

    added new, create, index, and show actions to events controller. Made views for events.

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,2 +1,28 @@
 class EventsController < ApplicationController
+
+  def index
+    @events = Event.all
+  end
+
+  def new
+    @event = Event.new
+  end
+
+  def create
+    @event = Event.new(event_params)
+    @event.save
+    redirect_to event_path(@event)
+  end
+
+  def show
+    @event = Event.find(params[:id])
+  end
+
+  protected
+
+  def event_params
+    params.require(:event).permit(:sport, :description, :datetime, :min_num_players, :max_num_players)
+  end
+
+
 end
